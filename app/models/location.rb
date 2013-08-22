8d3ceea4cf40d7575fb04a1a39901cb712130eb7
Zach Whitehead <zach@movable.com>
2013-08-22 14:03:18 -0400

    Initial upload

---
+++ app/models/location.rb
@@ -0,0 +1,26 @@
+class Location < ActiveRecord::Base
+  attr_accessible :address, :latitude, :longitude, :name
+  acts_as_gmappable :latitude => 'latitude', :longitude => 'longitude', :process_geocoding => :geocode?,
+
+	:address => "address", :normalized_address => "address",
+
+	:msg => "Sorry, not even Google could figure out where that is"
+
+	def geocode?
+
+	(!address.blank? && (latitude.blank? || longitude.blank?)) || address_changed?
+
+	end
+
+	def gmaps4rails_address
+
+	address
+
+	end
+
+	def gmaps4rails_infowindow
+
+	"<h4>#{name}</h4>" << "<h5>#{address}</h5>" << "<i>Made by Zach Whitehead</i>" 
+
+end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleMap::Application.config.secret_token = '02f9fb52028f85629669174199a4765f8281663de0e660dab47ca076f20b5428fa05b7bed72e9ca1d1162c1d7c4fb9a6e25a6392a658e9c6daa0cc6e23f388a8'
