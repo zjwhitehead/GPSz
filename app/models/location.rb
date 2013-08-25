43e077ccea92823e2e44e046b07b1d39ca4ee637
Zach Whitehead <zach@movable.com>
2013-08-25 16:38:29 -0400

    theming progress

--- app/models/location.rb
+++ app/models/location.rb
@@ -16,9 +16,10 @@ class Location < ActiveRecord::Base
 
 	def self.to_csv
 	  CSV.generate do |csv|
-	    csv << column_names
+	    csv << ["id","name", "location"]
 	    all.each do |location|
-	      csv << location.attributes.values_at(*column_names)
+	      csv << [location.id, location.name, location.address]
+
 	    end
 	  end
 	end
